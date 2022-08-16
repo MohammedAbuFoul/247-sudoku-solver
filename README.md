a6195b9d37f515efd3da20fdbfe380188c2f1abb
MohammedAboFoul <106703644+MohammedAbuFoul@users.noreply.github.com>
2022-08-16 10:27:41 +0100

    first working version
    you need to disable in game animations or increase the sleep timer to 1.5 seconds

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# 247 sudoku solver
- solver for 247sudoku.com  sudoku game
+247 sudoku solver written in python
+solver for 247sudoku.com  sudoku game
+
+Install these libraries from an administrator terminal (windows):
+
+pip install pywin32
+pip install keyboard
+pip install pyautogui
+pip install opencv-python
+
+
+# sudoku solving logic is copied with minor modifactions 
+# from Computerphile video titled 'Python sudoku solver'
+# video link https://youtu.be/G_UYXzGuqvM
--- main.py
+++ main.py
@@ -1,18 +1,70 @@
 import pyautogui
 import keyboard
 import win32api, win32con
+from time import sleep
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
+def possible(y, x, n):
+    global grid
+    for i in range (0, 9):
+        if grid[y][i] == n:
+            return False
+    for i in range (0, 9):
+        if grid[i][x] == n:
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0, 3):
+        for j in range (0, 3):
+            if grid[y0+i][x0+j] == n:
+                return False
+    return True
+
+
+def solve():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if grid[y][x] == 0:
+                for n in range (1, 10):
+                    if possible(y,x,n):
+                        grid[y][x] = n
+                        solve()
+                        grid[y][x] = 0
+                return
+    for g in grid:
+        print(g)
+        for a in g:
+            ans.append(a)
+
+
+
+def enter_key(x,y,k):
+    xpos = int(sudku_left+x*square_size+.5*square_size)
+    ypos = int(sudku_top+y*square_size+.5*square_size)
+    win32api.SetCursorPos((xpos,ypos))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+    sleep(.1)
+    keyboard.press_and_release(k)
+    sleep(.1)
+
     
-    
-def number_detact(x,y):
-    for num in range (1, 10, 1):
-        if pyautogui.locateOnScreen(f'pic//{num}.png', region=(sudku_left+x*square_size, sudku_top+y*square_size, square_size, square_size), grayscale=True, confidence=0.85) !=None:
-            return num
-    return 0
+def sudku_onscreen():
+    # convert the game to array
+    sudku_grid = []
+    for y in range (9):
+        row = []
+        for x in range (9):
+            for num in range (1,10,1):
+                not_found = True
+                if pyautogui.locateOnScreen(f'pic//{num}.png', region=(sudku_left+x*square_size, sudku_top+y*square_size, square_size, square_size), grayscale=True, confidence=0.85) !=None:
+                    row.append(num)
+                    not_found = False
+                    break
+            if not_found:
+                row.append(0)
+        sudku_grid.append(row)
+    return sudku_grid
 
 
 # gets the edge of the sudoku puzzle
@@ -31,12 +83,16 @@ sudku_width = dr[0]-ul[0]-16
 sudku_length = dr[1]-ul[1]-16
 square_size = int(sudku_width/9)
 
-# convert the game to array
-sudku_grid = []
-for y in range (9):
-    row = []
-    for x in range (9):
-        row.append(number_detact(x,y))
-    sudku_grid.append(row)
+# read the game on screen solve it then enter the correct solution
+ans = []
+grid = sudku_onscreen()
+for g in grid:
+    print(g)
 
-    
+solve()
+
+for y in range (9):
+    for x in range (9):
+        if grid[y][x] == 0:
+            enter_key(x,y,str(ans[y*9+x]))
+            
